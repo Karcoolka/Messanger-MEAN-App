26fb3bd98b088e35b5697ad095fd1492e2fbbb67
Adela Simkova <adela@sherpadevs.com>
2024-07-18 11:30:03 +0200

    Mongoose installed @ Deletion applied

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const Post = require('./models/post');
+
+mongoose.connect('mongodb+srv://adela:eqYA8DVGbouYnaeW@clustermeanapp.mlo9uoq.mongodb.net/node-angular?retryWrites=true&w=majority&appName=ClusterMEANApp')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,12 +29,29 @@ app.use((req, res, next) => {
   next();
 });
 
+// eqYA8DVGbouYnaeW
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
   });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
+  })
+
+});
+
+app.get('/api/posts', (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(201).json({
+        message: 'Post added successfully',
+        posts: documents
+      });
+    });
 });
 
 app.use('/api/posts', (req, res, next) => {
@@ -44,6 +72,11 @@ app.use('/api/posts', (req, res, next) => {
     message: 'Posts fetched successfully!',
     posts: posts
   });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  console.log(req.params.id);
+  res.status(200).json({ message: 'Post deleted!' });
 });
 
 
